759ac924ff23cc57d89da5977ced6c0dc6ce7465
shihab <echoshihab@gmail.com>
2019-06-20 21:03:10 -0400

    day 2 - css+JS clock completed

--- 02 - CSS + JS Clock/script.js
+++ 02 - CSS + JS Clock/script.js
@@ -1,2 +1,38 @@
-const time = new Date()
-console.log(time);
+
+
+clockFunc = () => {
+
+    const time = new Date()
+
+    // +90 accounts for the starting position of the hands
+    const secDegrees = (time.getSeconds())*6 + 90;
+    const minDegrees = (time.getMinutes())*6 + 90;
+    const hourDegrees = (time.getHours())*30 + 90;  //360/24 = 30
+
+
+    const minute= document.querySelector(".min");
+    const second = document.querySelector(".sec");
+    const hour = document.querySelector(".hour");
+
+
+// this block of if code takes care of the rewind of hands to starting position at 0sec/min/hour
+         if (secDegrees === 90) {
+       second.style.transition = "0.0s"
+    }
+
+         if (minDegrees === 90) {
+       second.style.transition = "0.0s"
+    }
+
+        if (hourDegrees === 90) {
+       second.style.transition = "0.0s"
+    }
+
+
+    minute.style.transform = `rotate(${minDegrees}deg)`;
+    hour.style.transform = `rotate(${hourDegrees}deg)`;
+    second.style.transform = `rotate(${secDegrees}deg)`;
+
+}
+
+window.setInterval(clockFunc, 1000)
---
+++ README.md
@@ -0,0 +1,10 @@
+Starting Wes Bos's Javascript 30 days of challenges from here : [JavaScript30](https://javascript30.com/)
+
+# Day 1: JavaScript Drump Kit : Completed
+I tried to recreate the starter files using CSS GRID for practice, and used deep house drum samples instead of the samples provided. House drum beats were taken from [Soundpacks](https://soundpacks.com/free-sound-packs/deep-house-drum-samples/).
+
+# Day 2: CSS+ JS clock: Completed
+Recreated the starter files from scratch and solved the issue with clock hands rewinding to starting position at zero sec/min/hour
+
+
+
