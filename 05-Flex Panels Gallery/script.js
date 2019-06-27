a5a5e0cb9710e797769aeb1aa9e7b0279ffea6b8
shihab <echoshihab@gmail.com>
2019-06-26 21:31:06 -0400

    complete day 5-flex panels gallery challenge

--- 05-Flex Panels Gallery/script.js
+++ 05-Flex Panels Gallery/script.js
@@ -2,9 +2,10 @@ const boxes = document.querySelectorAll('.box');
 
 
 const updateBox = () => {
-    event.target.children[0].style.transform= 'translateY(0px)';
+    event.target.children[0].style.transform= 'translateY(0%)';
+    event.target.children[1].style.transform = 'translateY(1200%)';
     event.target.style.flex = "5";
 }
 
 
-boxes.forEach(box => box.addEventListener("mousedown", updateBox))
+boxes.forEach(box => box.addEventListener("click", updateBox))
