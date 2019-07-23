6c7fa858a0a61c421ff08b57828df086b9e4a9ff
shihab <echoshihab@gmail.com>
2019-07-23 07:36:30 -0400

    work on day 15+solve clear all problem

--- 15 - LocalStorage + Event Delegation/script.js
+++ 15 - LocalStorage + Event Delegation/script.js
@@ -1,15 +1,51 @@
-const input_text = document.querySelector(".add-item");
-const submit_button = document.querySelector(".submit-item");
-const todo = document.querySelector(".todo")
 
-console.log(submit_button)
+const todo = document.querySelector(".todo");
+const ul = document.querySelector(".item-list");
+const items = JSON.parse(localStorage.getItem('items')) || [];
+const clear = document.querySelector(".clear");
+const check = document.querySelector(".check");
+const uncheck = document.querySelector(".uncheck");
 
 function addItem(e) {
-    console.log('firing')
     e.preventDefault();
-    console.log(input_text.value);
+    text = this.querySelector(".add-item").value
+    const item = {
+        text,
+        done: false
+    }
+
+    items.push(item);
+    populateList(items, ul);
+    localStorage.setItem('items', JSON.stringify(items));
     todo.reset();
 }
 
-submit_button.addEventListener('onsubmit', addItem);
+
+function populateList (todos = [], list) {
+    list.innerHTML = todos.map((todo, index) => {
+        return `<li>
+            <input type="checkbox" data-index=${index} id="item${index}" ${todo.done ? 'checked' : ''} />
+            <label for="item${index}">${todo.text}</label>
+            </li>`;}).join('');
+    }
+
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; //skip if it is not an input
+    const el = e.target
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, ul);
+}
+
+function clearAll(e) {
+    localStorage.removeItem('items');
+    ul.innerHTML="";
+}
+
+ul.addEventListener('click', toggleDone)
+todo.addEventListener('submit', addItem);
+clear.addEventListener('click', clearAll )
+populateList(items, ul);
 
