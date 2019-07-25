3aefe134270393ce1bcdd0f2e0c9b8771069dc1c
shihab <echoshihab@gmail.com>
2019-07-24 21:21:52 -0400

    text shadow is complete on mouse move, but looks glitchy

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -2,8 +2,24 @@ const h1 = document.querySelector('h1')
 
 
 function mouseTracker(e) {
-    [x, y] = [e.offsetX, e.offsetY]
+    [x, y] = [e.offsetX, e.offsetY];
+    x = x - h1.offsetTop;
+    y = y - h1.offsetLeft;
+
     console.log(x, y)
+
+    if (x > 100){
+        x = 100
+    } else if (x < -100) {
+        x = -100
+    }
+
+    if (y > 100) {
+        y = 100
+    } else if ( y < -100) {
+        y = -100
+    }
+
     h1.style.textShadow = `${x}px ${y}px #FF0000`
 
 }
